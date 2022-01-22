9f6d25eefa9f93d48626523100b39a2f5912d8e4
Haoran Zhou <kens0serious@gmail.com>
2022-01-22 17:05:54 -0500

    completed next, pre button functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import people from './data.js'
 
 const container = document.querySelector('.slider-container')
+const prev = document.querySelector('.prev-btn')
+const next = document.querySelector('.next-btn')
 
 container.innerHTML = people
   .map((ppl, index) => {
@@ -31,3 +33,41 @@ container.innerHTML = people
         </article>`
   })
   .join('')
+
+const startSlider = (type) => {
+  console.log('hello')
+  console.log(type)
+
+  const active = document.querySelector('.active')
+  const last = document.querySelector('.last')
+  let next = active.nextElementSibling
+
+  if (!next) {
+    next = container.firstElementChild
+  }
+
+  active.classList.remove(['active'])
+  last.classList.remove(['last'])
+  next.classList.remove(['next'])
+
+  if (type === 'prev') {
+    active.classList.add('next')
+    last.classList.add('active')
+    next = last.previousElementSibling
+    if (!next) {
+      next = container.lastElementChild
+    }
+    next.classList.remove(['next'])
+    next.classList.add('last')
+
+    return
+  }
+
+  next.classList.add('active')
+  last.classList.add('next')
+  active.classList.add('last')
+}
+
+next.addEventListener('click', () => startSlider())
+
+prev.addEventListener('click', () => startSlider('prev'))
